cabcc4a0a86859f93e35b9b94b88babf5ccee3ab
Clara Monteiro <S00185743@mail.itsligo.ie>
2020-01-23 16:35:18 +0000

    Add project files.

---
+++ Lab_Sheet_1/Band.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Sheet_1
+{
+    class Band : IComparable
+    {
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public string Members { get; set; }
+
+        public Band (string name, int year, string members)
+        {
+            Name = name;
+            Year = year;
+            Members = members;
+        }
+
+        public Band()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Year} {Members}";
+        }
+
+        public int CompareTo(Band other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab_Sheet_1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_Sheet_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Band> AllBands = new List<Band>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Band b1 = new Band()
+            {
+                Name = "Arctic Monkeys",
+
+            };
+            Band b2 = new Band()
+            {
+                Name = "The Beatles",
+
+            };
+            Band b3 = new Band()
+            {
+                Name = "BTS",
+
+            };
+            Band b4 = new Band()
+            {
+                Name = "GOT7",
+
+            };
+           
+        }
+    }
+}
